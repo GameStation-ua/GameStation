8442dd2366b9180241c00881f615a638d5f6540d
JorgeSuarezV <jorge.sv.primero@gmail.com>
2022-04-16 17:20:09 -0300

    cors arreglado(creo)

--- src/main/java/webpage/GameStation.java
+++ src/main/java/webpage/GameStation.java
@@ -5,15 +5,15 @@ import webpage.handlers.*;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
-import static spark.Spark.port;
-import static spark.Spark.staticFiles;
+import static spark.Spark.*;
 import static webpage.util.Paths.*;
 
 public class GameStation {
 
     public static void main(String[] args) {
-        staticFiles.location("/public");
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("GameStation");
+        enableCORS("*","Origin, X-Requested-With, Content-Type, Accept, Authorization, token, imgType, gameId","GET,POST,PUT,DELETE,OPTIONS,HEAD");
+        staticFiles.location("/public");
         port(8443);
         new RegisterHandler(emf).handle(register);
         new LogInHandler(emf).handle(logIn);
@@ -21,5 +21,30 @@ public class GameStation {
         new HomeHandler(emf).handle(home);
         new UploadHandler(emf).handle(upload);
         new MyHandler(emf).handle("/main");
+    }
+
+    private static void enableCORS(final String origin, final String methods, final String headers) {
+
+        options("/*", (request, response) -> {
+
+            String accessControlRequestHeaders = request.headers("Access-Control-Request-Headers");
+            if (accessControlRequestHeaders != null) {
+                response.header("Access-Control-Allow-Headers", accessControlRequestHeaders);
+            }
+
+            String accessControlRequestMethod = request.headers("Access-Control-Request-Method");
+            if (accessControlRequestMethod != null) {
+                response.header("Access-Control-Allow-Methods", accessControlRequestMethod);
+            }
+
+            return "OK";
+        });
+
+        before((request, response) -> {
+            response.header("Access-Control-Allow-Origin", origin);
+            response.header("Access-Control-Request-Method", methods);
+            response.header("Access-Control-Allow-Headers", headers);
+            response.type("application/json");
+        });
     }
 }
