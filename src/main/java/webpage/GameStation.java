a2febafab73c901b3b8872645ec02762ab91ee63
JorgeSuarezV <jorge.sv.primero@gmail.com>
2022-05-19 16:09:57 -0300

    main un poco más prolijo

--- src/main/java/webpage/GameStation.java
+++ src/main/java/webpage/GameStation.java
@@ -2,26 +2,18 @@ package webpage;
 
 import webpage.handlers.MyHandler;
 import webpage.handlers.NotificationHandler;
-import webpage.util.EntityManagers;
 import webpage.util.Handler;
 import webpage.util.HandlerProvider;
 import webpage.util.HandlerProviderImpl;
 
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.Persistence;
-
 import static spark.Spark.*;
+import static webpage.util.ServerInitializer.*;
 
 public class GameStation {
 
     public static void main(String[] args) {
-        EntityManagerFactory emf = Persistence.createEntityManagerFactory("GameStation");
-        EntityManagers.setFactory(emf);
-//        staticFiles.location("/public");
-        String projectDir = System.getProperty("user.dir");
-        String staticDir = "/src/main/resources/public";
-        staticFiles.externalLocation(projectDir + staticDir);
-        // use location on real server
+        generateEMF();
+        setStaticFilesDirectory("/src/main/resources/public");
         webSocket("/notifications", NotificationHandler.class);
         port(8443);
         enableCORS();
@@ -30,20 +22,4 @@ public class GameStation {
         for (final Handler handler : handlers) handler.handle();
         new MyHandler().handle();
     }
-
-    private static void enableCORS() {
-
-        options("/*", (request, response) -> {
-            response.status(200);
-            return "OK";
-        });
-
-        before((request, response) -> {
-            response.header("Access-Control-Allow-Origin", "*");
-            response.header("Access-Control-Request-Method", "*");
-            response.header("Access-Control-Allow-Headers", "*");
-            response.type("application/json");
-        });
-    }
-
 }
---
+++ src/main/java/webpage/util/ServerInitializer.java
@@ -0,0 +1,45 @@
+package webpage.util;
+
+import webpage.handlers.NotificationHandler;
+
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import static spark.Spark.*;
+
+public class ServerInitializer {
+
+    public static void initializeHandlers(){
+        webSocket("/notifications", NotificationHandler.class);
+        HandlerProvider handlerProvider = new HandlerProviderImpl();
+        final Iterable<Handler> handlers = handlerProvider.getAllHandlers();
+        for (final Handler handler : handlers) handler.handle();
+    }
+
+    public static void setStaticFilesDirectory(String directory){
+        //        staticFiles.location("/public");
+        String projectDir = System.getProperty("user.dir");
+        staticFiles.externalLocation(projectDir + directory);
+        // use location on real server
+    }
+
+    public static void generateEMF(){
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("GameStation");
+        EntityManagers.setFactory(emf);
+    }
+
+    public static void enableCORS() {
+
+        options("/*", (request, response) -> {
+            response.status(200);
+            return "OK";
+        });
+
+        before((request, response) -> {
+            response.header("Access-Control-Allow-Origin", "*");
+            response.header("Access-Control-Request-Method", "*");
+            response.header("Access-Control-Allow-Headers", "*");
+            response.type("application/json");
+        });
+    }
+}
