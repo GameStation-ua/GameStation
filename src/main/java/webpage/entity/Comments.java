f8cbd1e02c7f59a050a1c40edfbacec817b83c87
JorgeSuarezV <jorge.sv.primero@gmail.com>
2022-06-28 13:08:35 -0300

    Comments

--- src/main/java/webpage/entity/Comments.java
+++ src/main/java/webpage/entity/Comments.java
@@ -15,16 +15,16 @@ import static webpage.util.EntityManagers.currentEntityManager;
 
 public class Comments {
 
-    public static Optional<Integer> findCommentVotesByUserIdAndCommentId(Long userId, Long commentId){
+    public static Integer findCommentVotesByUserIdAndCommentId(Long userId, Long commentId){
         EntityManager em = currentEntityManager();
         try {
             Integer vote = (Integer) em.createQuery("SELECT vote FROM UserComment uc WHERE uc.userId = ?1 AND uc.commentId = ?2")
                     .setParameter(1, userId)
                     .setParameter(2, commentId)
                     .getSingleResult();
-            return Optional.of(vote);
+            return vote;
         }catch (Exception e){
-            return Optional.empty();
+            return 0;
         }finally {
             close();
         }
@@ -48,17 +48,13 @@ public class Comments {
         List<CommentResponse> commentResponseList = new ArrayList<>();
         for (Comment comment : comments) {
 
-            Optional<Integer> vote = findCommentVotesByUserIdAndCommentId(userId, comment.getId());
+            Integer vote = findCommentVotesByUserIdAndCommentId(userId, comment.getId());
 
             Optional<User> user = findUserById(userId);
 
             if (user.isEmpty()) continue;
-            if (vote.isEmpty()) {
-                commentResponseList.add(new CommentResponse(comment, user.get().getNickname(), 0));
-                continue;
-            }
 
-            commentResponseList.add(new CommentResponse(comment, user.get().getNickname(), vote.get()));
+            commentResponseList.add(new CommentResponse(comment, user.get().getNickname(), vote));
         }
         return commentResponseList;
     }
